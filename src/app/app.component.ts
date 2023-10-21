ab16bcd4a45119e66e6f563029ff4dfd755ef8d7
jajet.loh <jajetloh@gmail.com>
2023-10-22 06:12:08 +0800

    first working version, with about me and projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { MatChipListboxChange } from "@angular/material/chips"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+
+enum FilterValues {
+    LearningResource = 'Learning Resource',
+    Code = 'Code',
+    Application = 'Application',
+    ResearchThesis = 'Document',
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio-app';
+    title = 'portfolio-app'
+
+    FilterValues = FilterValues
+
+
+    filterValue?: FilterValues
+
+    faLinkIcon = faArrowUpRightFromSquare
+
+
+    applyFilter(event: MatChipListboxChange) {
+        this.filterValue = (event.value as FilterValues | undefined)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatChipsModule } from "@angular/material/chips";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatChipsModule,
+        FontAwesomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
