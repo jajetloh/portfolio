1a35e91654c4d1135938e2910b299e9b99dccb15
Ja-Jet Loh <jajet.codes@gmail.com>
2023-12-13 20:57:17 +0800

    remove unnecessary imports

--- src/app/benchmarking-polars-vs-pandas/benchmarking-polars-vs-pandas.component.ts
+++ src/app/benchmarking-polars-vs-pandas/benchmarking-polars-vs-pandas.component.ts
@@ -1,15 +1,14 @@
 import {Component, OnInit} from '@angular/core'
 import {
     faAngleDoubleDown, faAngleDown, faAngleUp, faArrowDown, faArrowUp,
-    faArrowUpRightFromSquare, faChartSimple,
+    faChartSimple,
     faCheck, faChevronDown,
     faChevronUp,
-    faCircleChevronUp,
     faXmark
 } from "@fortawesome/free-solid-svg-icons"
 import { faCircle } from "@fortawesome/free-regular-svg-icons"
 import * as performanceData from '../../assets/performance-results.json'
-import {groupBy, mean, sum} from 'lodash-es'
+import {groupBy, sum} from 'lodash-es'
 import {ViewportScroller} from "@angular/common";
 
 @Component({
